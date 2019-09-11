0f3af8b8538ed81d230e1270de6d098c2820c71c
hakimmaaouia <hakimmaaouia@gmail.com>
2019-09-11 02:34:10 +0100

    2

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -4,61 +4,16 @@ import Bot from './bot';
 import Main from './main';
 
 
-/* geo*/
 
-const Home = (aux) => {
-
-  const [lat,setlat] = useState(0);
-  const [long, setlong] = useState(0);
- /* geo*/
-
-
+  render() { 
+  return (
+        
   
- useEffect(() => { 
-
-    const showPosition = (position) => {    
-      setlat(position.coords.latitude);
-      setlong(position.coords.longitude); 
-    }
-     
-    const  getLocation = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition)
-   
-        
-        
-      } else { 
-        console.log("false");
-        
-      }
-      
-    }
-    getLocation();
-  }, []);
-  
-/* geo*/
-  /*loc*/
-const loc = (aux) => {
-  var r = ""   
-  if (typeof aux.location.state === 'undefined') {   
-      r="lat="+lat+"&lon="+long;
-      
-  }else
-  {
-      r="q="+aux.location.state.info
-  }
-  return r;
+    <div>
+    <div className="bg"><Main /></div>
+   <div className="bgb"> <Bot/></div>
+   </div>
+);
 }
-/*loc*/
-console.log(loc(aux));
 
-    return (
-        
-  
-        <div>
-        <div className="bg"><Main location={loc(aux)}/></div>
-       <div className="bgb"> <Bot/></div>
-       </div>
-    );
-} 
 export default Home;
