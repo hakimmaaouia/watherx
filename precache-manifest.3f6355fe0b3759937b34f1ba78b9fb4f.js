d050971e8f152104147cf64cb319215312ffddd8
hakimmaaouia <hakimmaaouia@gmail.com>
2019-09-12 23:37:02 +0100

    Updates

---
+++ precache-manifest.3f6355fe0b3759937b34f1ba78b9fb4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e021993739c0faf2e622bb09e36388f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e23581591a3d61767ebf",
+    "url": "/static/css/main.82f00def.chunk.css"
+  },
+  {
+    "revision": "7c5033657723188a8a89",
+    "url": "/static/js/2.e19c6f73.chunk.js"
+  },
+  {
+    "revision": "e23581591a3d61767ebf",
+    "url": "/static/js/main.928af2b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/static/media/search-solid.c6a0effb.svg"
+  }
+]);
